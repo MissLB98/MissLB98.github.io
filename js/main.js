588a589b7b997295d9948c2f468bc0f47837bbcc
Leah Birch <goldbeat98@gmail.com>
2018-03-09 17:52:36 +0200

    tried adding a modal box. still busy

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 //JS
 
 const projectsContainer = document.getElementById('projects');
-const button = document.getElementById('button');
 
 //project data
 const data = [
@@ -9,30 +8,35 @@ const data = [
 		alt: "Project: Employee Directory",
 		link: "projects/employeeDirectory/index.html",
 		img: "img/logo3.png",
+		moreInfo: () =>	{
+			return vex.dialog.alert({ unsafeMessage: '<b>ello!</b>' });;
+		},
 	},
 	{
 		alt: "Project: Interactive Media Player",
 		link: "projects/mediaPlayer/index.html",
 		img: "img/logo2.png",
+		moreInfo: () =>	{
+			return vex.dialog.alert({ unsafeMessage: '<b>Hello!</b>' });;
+		},
 	},
 	{
 		alt: "Project: Lightbox Photo Gallery",
 		link: "projects/photoGallery/index.html",
 		img: "img/logo4.png",
+		moreInfo: function ()	{
+			return vex.dialog.alert({ unsafeMessage: '<b>Hello!</b>' });
+		},
 	},
 	{
 		alt: "Project: Educare",
 		link: "projects/educare/index.html",
 		img: "img/logo1.png",
+		moreInfo: function ()	{
+			return vex.dialog.alert({ unsafeMessage: '<b>Hello!</b>' });
+		},
 	},	
 ];
-
-
-//info pop up
-
-function popup()	{
-	
-}
 
 
 //building the project html
@@ -41,16 +45,20 @@ let HTML = '<ul>';
 for (let i = 0; i < data.length; i++)	{
 	HTML += '<li>';
 		HTML += '<a href="' + data[i].link + '" target="_blank">';
-			HTML += '<img src="' + data[i].img + '" alt="' + data[i].alt + '" />';
+			HTML += '<img class="p-img" src="' + data[i].img + '" alt="' + data[i].alt + '" />';
 		HTML += '</a>';
-		HTML += '<input id="button" type="button" value="Click here to read more" />';
+		
+		function dialog()	{
+			return data[i].moreInfo();
+		}
+		
+		HTML += '<button onclick="dialog()" id="button" type="button">Click here to read more</button>';
 	HTML += '</li>';
 }
+
 
 HTML += '</ul>';
 
 
-
-
-
 projectsContainer.innerHTML = HTML;
+
